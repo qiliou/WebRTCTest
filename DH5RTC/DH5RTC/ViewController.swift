068552e3e5faf59b776f9fbdd4b668c5d4e73057
Chuanqi <design@Gifts-iMac.local>
2017-10-10 10:31:43 -0700

    add projectaaaa

---
+++ DH5RTC/DH5RTC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DH5RTC
+//
+//  Created by Chuanqi on 2017/10/10.
+//  Copyright © 2017年 Gift Drop Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
